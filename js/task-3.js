fcb7a81a59b373c2562bd7677e29f4865b825639
Artem Strelnikov <piramida1237@gmail.com>
2026-09-20 11:22:28 +0200

    final project completion

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,11 @@
+function getElementWidth(content, padding, border) {
+  const namContent = Number.parseFloat(content);
+  const namPadding = Number.parseFloat(padding);
+  const namBorder = Number.parseFloat(border);
 
+  return namContent + namPadding * 2 + namBorder * 2;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
